f45d8c2d865296f2c3f0178d41acd11c6b097626
Ainis Taranda <taranda.ainis@gmail.com>
2022-11-01 15:57:21 -0400

    automatically showing new albums

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react'
 import AlbumCard from './AlbumCard'
 
 
-export default function AlbumList() {
+export default function AlbumList({ toggle }) {
     const [albums, setAlbums] = useState()
     useEffect(() => {
         fetch('https://albums-api-at.web.app/albums')
         .then(response => response.json())
         .then(setAlbums)
         .catch(alert)
-    }, [])
+    }, [toggle])
     return (
         <main className='album-list'>
             {!albums
